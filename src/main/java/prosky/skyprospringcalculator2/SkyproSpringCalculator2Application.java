bc1a7d5783086ea98569eddfe7eb25ed2bda0a8b
margo <margo171190@mail.ru>
2023-07-09 17:29:58 +0400

    Initial commit

---
+++ src/main/java/prosky/skyprospringcalculator2/SkyproSpringCalculator2Application.java
@@ -0,0 +1,13 @@
+package prosky.skyprospringcalculator2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringCalculator2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringCalculator2Application.class, args);
+	}
+
+}
